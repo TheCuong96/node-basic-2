ae2580d69c5b1715e5526e679d4e7f7189fc2599
thecuong1396 <cuongphan@coinmap.tech>
2023-04-24 01:16:14 +0700

    route with MVC

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+import initWebRoute from "./routes/web";
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8080;
 
 configViewEngine(app);
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-});
-app.get("/about", (req, res) => {
-    res.send(`I'm Eric!`);
-});
+//init web route
+initWebRoute(app);
+
+// app.get("/", (req, res) => {
+//     res.render("index.ejs");
+// });
+// app.get("/about", (req, res) => {
+//     res.send(`I'm Eric!`);
+// });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
