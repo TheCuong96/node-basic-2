f4d05cf1a1c607add7e2c9afce117c4f82386311
thecuong1396 <cuongphan@coinmap.tech>
2023-04-23 20:56:10 +0700

    static files

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,4 +1,7 @@
+import express from "express";
+
 const configViewEngine = (app) => {
+    app.use(express.static("./src/public"));
     app.set("view engine", "ejs"); // thằng này để nó xác định rằng các view engine, còn gọi cách khác là các code html mà node có thể đọc được là phải thông qua file có đuôi là ejs
     app.set("views", "./src/views"); // thằng này để xác định rằng những view UI ta viết bằng html sẽ được đọc trong thư mục là ./src/views
 };
