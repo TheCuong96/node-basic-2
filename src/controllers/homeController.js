eafd34596df1bf0721950da877281d8e72d46aa0
thecuong1396 <cuongphan@coinmap.tech>
2023-04-25 00:18:23 +0700

    EJS LOOP

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,9 +2,8 @@ import connection from "../configs/connectDB";
 
 let getHomepage = (req, res) => {
     //logic
-    let data = [];
     connection.query("SELECT * FROM `users` ", function (err, results, fields) {
-        return res.render("index.ejs", { dataUser: JSON.stringify(results) });
+        return res.render("index.ejs", { dataUser: results });
     });
 };
 
