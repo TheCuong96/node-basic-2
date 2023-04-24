a921cad8cb8a986bece8d5cd01aad7abb1912f65
thecuong1396 <cuongphan@coinmap.tech>
2023-04-25 03:25:06 +0700

    create data

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8080;
 
+//2 thằng dưới đây dùng để compile code lại code js mà ta có thể hiểu và sử dụng được ở node mỗi khi có data được bắn qua lại giữa server và client
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 configViewEngine(app);
 
 //init web route
