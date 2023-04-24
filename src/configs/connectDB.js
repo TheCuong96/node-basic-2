66169949a91df0fb6ffc7d876cd84acc7694cd5f
thecuong1396 <cuongphan@coinmap.tech>
2023-04-25 01:24:11 +0700

    sử dụng createPool để xử lý cache cho server

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,11 +1,25 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise"; // sử dụng mysql2/promise để có thể lấy createPool
+// // create the connection to database
+console.log("Creating connection pool...");
 
-// create the connection to database
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
+    // createPool có hỗ trợ cache cho ta, để tránh người dùng reload page liên tục và server phải trả lại dữ liệu liên tục, nên sử dụng createPool để có thể dùng pool.execute để nó lưu trữ data đệm lại khi bị gọi nhiều lần mà cùng 1 data giống nhau
     host: "localhost",
     user: "root",
     database: "nodejsbasic",
+    // password: 'password'
 });
+
+export default pool;
+
+// import mysql from "mysql2";
+
+// // create the connection to database
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     database: "nodejsbasic",
+// });
 
 // // simple query
 // connection.query(
@@ -19,4 +33,4 @@ const connection = mysql.createConnection({
 //     }
 // );
 
-export default connection;
+// export default connection;
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,20 @@
-import connection from "../configs/connectDB";
+import pool from "../configs/connectDB";
 
-let getHomepage = (req, res) => {
-    //logic
-    connection.query("SELECT * FROM `users` ", function (err, results, fields) {
-        return res.render("index.ejs", { dataUser: results });
-    });
+let getHomepage = async (req, res) => {
+    const [rows, fields] = await pool.execute("SELECT * FROM users");
+
+    return res.render("index.ejs", { dataUser: rows, test: "abc string test" });
+};
+
+let getDetailPage = async (req, res) => {
+    let userId = req.params.id;
+    let [user] = await pool.execute(`select * from users where id = ?`, [
+        userId,
+    ]);
+    return res.send(JSON.stringify(user));
 };
 
 module.exports = {
     getHomepage,
+    getDetailPage,
 };
